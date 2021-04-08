95c6513d87c55d7021e2ef676dc8946cb7e6d288
Tasha <navancini@gmail.com>
2021-04-08 12:10:33 -0400

    home page

---
+++ src/containers/Planter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Planter extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Home Page</h1>
+      </div>
+    )
+  }
+}
+
+export default Planter
