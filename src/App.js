e3fc642dd2adcf8a6159dcb4e25b4df2f6035188
Tasha <navancini@gmail.com>
2021-04-08 12:06:48 -0400

    added routes and imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { getPlants } from './actions/plantdex';
+import Navbar from './containers/Navbar';
+import Footer from './containers/Footer';
+import Planter from './containers/Planter';
+import ErrorPage from './containers/Error';
+import Plantdex from './components/Plantdex';
+import About from './containers/About';
+import Form from './components/Plantform';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.getPlants();
+  }
+
+  render() {
+    if (this.props.loading) {
+      return (
+        <h2> Loading... </h2>
+      )
+    }
+    return (
+      <Router> 
+       <Navbar /> 
+        <Switch>
+         <Route exact path="/" component={ Planter } />
+         <Route exact path="/about" component={ About } />
+         <Route exact path="/plantdex" component={ Plantdex } />
+         <Route exact path="/plantdex/new" component={ Form } />
+         <Route component={ErrorPage} />
+        </Switch>
+       <Footer/>
+      </Router>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    loading: state.loading
+  }
+}
+
+export default connect(mapStateToProps, { getPlants })(App);
