00a14553df05614a0a9c13b5909cc1b122f19d9a
Tasha <navancini@gmail.com>
2021-04-08 12:10:16 -0400

    placeholder footer stateless

---
+++ src/containers/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+        
+        ©Soupy Inc. 
+      </div>
+    )
+  }
+}
+
+export default Footer
