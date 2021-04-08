675c5e5131d08f559c101c71ecc9ca7ca8b4ed1c
Tasha <navancini@gmail.com>
2021-04-08 12:09:52 -0400

    placeholder about stateless comp

---
+++ src/containers/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1> About this dumbass app</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper eget nulla facilisi etiam dignissim diam quis. Suspendisse interdum consectetur libero id faucibus nisl. Id semper risus in hendrerit. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Cursus in hac habitasse platea dictumst quisque sagittis. Nunc eget lorem dolor sed viverra ipsum nunc. Phasellus egestas tellus rutrum tellus pellentesque eu. Arcu felis bibendum ut tristique et egestas. Enim lobortis scelerisque fermentum dui faucibus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Suspendisse interdum consectetur libero id faucibus nisl tincidunt. Commodo odio aenean sed adipiscing. Egestas erat imperdiet sed euismod nisi porta lorem. Enim facilisis gravida neque convallis a cras semper. Mauris cursus mattis molestie a iaculis. </p>
+      </div>
+    )
+  }
+}
+
+export default About
