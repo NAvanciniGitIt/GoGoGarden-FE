6d8b66926acc7100b7449f30c56bc130e66314d6
Tasha <navancini@gmail.com>
2021-04-09 10:45:37 -0400

    styling

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 export class Footer extends Component {
   render() {
     return (
-      <div>
-        
-        ©Soupy Inc. 
-      </div>
+      <div class="footer-copyright">
+            <div class="container">
+            © 2021 Soupy Inc. 
+            </div>
+          </div>
     )
   }
 }
+
 
 export default Footer
--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -4,16 +4,21 @@ import { Link } from 'react-router-dom';
 export class Navbar extends Component {
   render() {
     return (
-      <div>
-        <ul>
+     <nav> 
+      <div class="nav-wrapper">
+      <Link to="/" class="brand-logo center">GoGoGarden</Link>
+        <ul id="nav" class="right">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/plantdex">Plantdex</Link></li>
           <li><Link to="/plantdex/new">Add Plant</Link></li>
         </ul>
       </div>
+    </nav>  
     )
   }
 }
+
+
 
 export default Navbar
