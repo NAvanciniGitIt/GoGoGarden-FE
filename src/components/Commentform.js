baf23a08dc00247c8dc3cfa10b4aa4b1cbec4102
Tasha <navancini@gmail.com>
2021-04-12 13:50:13 -0400

    comment form -WIP

---
+++ src/components/Commentform.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addComment } from '../actions';
+// import { addPlant } from '../actions';
+
+export class Commentform extends Component {
+
+  state = {
+    content: "",
+    plant_id: this.props.plant_id
+
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addComment(this.state, this.props.history)
+    this.setState({
+      content: "",
+      plant_id: this.props.plant_id
+    })
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <form onSubmit={ this.handleSubmit }> 
+         <h4 className="center"><label htmlFor="comment">Comment</label></h4>
+         <input className="commentBox" type="text" name="content" value={this.state.content} onChange={ this.handleChange }/>
+         <div className="container center">
+            <button>Submit</button>
+         </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect(null, { addComment})(Commentform); 
