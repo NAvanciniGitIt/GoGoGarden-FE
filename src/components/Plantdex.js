43ca589196cfcf3022c807e4ba9a2328849792bd
Tasha <navancini@gmail.com>
2021-04-12 13:50:39 -0400

    a little refactor with added links to indiv ids

--- src/components/Plantdex.js
+++ src/components/Plantdex.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Plants from '../containers/Plants';
+//import Plants from '../containers/Plants';
+import { Link } from 'react-router-dom'
 
 
 export class Plantdex extends Component {
+  
+    
   render() {
-    const plants = this.props.plants.map( (plant, i) => <Plants key={i} plant_type= {plant.plant_type} seed_type={plant.seed_type} seed_date={plant.seed_date} light={plant.light} environment={plant.environment} soil={plant.soil} notes={plant.notes} problems={plant.problems} />)
-    return (
-      <div>
-        { plants }
-      </div> 
+   
+    const { plants } = this.props
+    
+    const plantsList = plants.length ? (
+      plants.map(plant => {
+        return (
+          <div className="plants" key={plant.id}>
+            <div className="card" >
+              <div className="card-content">
+                <Link to={'/' + plant.id}>
+                  <span className="card-title">{plant.plant_type}: {plant.seed_type}</span> 
+                </Link>
+              </div>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">None yet</div>
     )
+    return (
+      <div className="container">
+        <h4 className="center">Plants</h4>
+        {plantsList}
+      </div>
+    )
+  
+    
   }
 }
 
@@ -21,3 +46,6 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(Plantdex);
+
+
+// const plants = this.props.plants.map( (plant, i) => <Plants key={i} plant_type= {plant.plant_type} seed_type={plant.seed_type} seed_date={plant.seed_date} light={plant.light} environment={plant.environment} soil={plant.soil} notes={plant.notes} problems={plant.problems} />)
